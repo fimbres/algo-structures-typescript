360564823ca0dc6836b0f884881047afd182d63a
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-12-04 22:40:07 -0800

    traversing binary trees methods

--- data-structures/binary-search-tree.ts
+++ data-structures/binary-search-tree.ts
@@ -80,8 +80,104 @@ class BinarySearchTree {
       }
     }
   }
+
+  BFS() { // Visit every node in each leavel before moving down to the next level
+    if(!this.root) return null;
+
+    const queue: TNode[] = [];
+    const visited: number[] = [];
+
+    queue.unshift(this.root!);
+
+    while(!!queue.length) {
+      let nodeOut = queue.pop();
+
+      visited.push(nodeOut?.value!);
+
+      if(!!nodeOut?.left) {
+        queue.unshift(nodeOut?.left);
+      }
+
+      if(!!nodeOut?.right) {
+        queue.unshift(nodeOut?.right);
+      }
+    }
+
+    return visited;
+  }
+
+  preOrderDFS() { // Visit every node from the left untill reaching the bottom, then going to the right.
+    if(!this.root) return null;
+
+    let visited: number[] = [];
+    let current = this.root;
+
+    function helper(node: TNode) {
+      visited.push(node.value);
+
+      if(node.left) {
+        helper(node.left);
+      }
+
+      if(node.right) {
+        helper(node.right);
+      }
+    }
+
+    helper(current);
+
+    return visited;
+  }
+
+  postOrderDFS() { // traverse every node to the left, then to the right, and then visit the current node.
+    if(!this.root) return null;
+
+    let visited: number[] = [];
+    let current = this.root;
+
+    function helper(node: TNode) {
+      if(node.left) {
+        helper(node.left);
+      }
+
+      if(node.right) {
+        helper(node.right);
+      }
+
+      visited.push(node.value);
+    }
+
+    helper(current);
+
+    return visited;
+  }
+
+  inOrderDFS() { // traverse all the nodes in the left, visit the current node, and then traverse all the way down from the right.
+    if(!this.root) return null;
+
+    let visited: number[] = [];
+    let current = this.root;
+
+    function helper(node: TNode) {
+      if(!!node.left) {
+        helper(node.left);
+      }
+
+      visited.push(node.value);
+
+      if(!!node.right) {
+        helper(node.right);
+      }
+    }
+
+    helper(current);
+
+    return visited;
+  }
 }
 
-let bst = new BinarySearchTree().insert(10).insert(5).insert(13).insert(2).insert(7).insert(11).insert(16);
-console.log(bst.find(16));
-console.log(bst.root?.value, bst.root?.left?.value, bst.root?.right?.value)
+let bst = new BinarySearchTree().insert(10).insert(6).insert(15).insert(3).insert(8).insert(20);
+console.log(bst.BFS()); // [ 10, 6, 15, 3, 8, 20 ]
+console.log(bst.preOrderDFS()); // [ 10, 6, 3, 8, 15, 20 ]
+console.log(bst.postOrderDFS()); // [ 3, 8, 6, 20, 15, 10 ]
+console.log(bst.inOrderDFS()); // [ 3, 6, 8, 10, 15, 20 ] 
