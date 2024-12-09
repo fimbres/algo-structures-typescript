a3814189811bd99cb96e4ab3f9309171d9e24ad9
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-12-08 18:19:47 -0800

    traversing graphs

--- data-structures/graph.ts
+++ data-structures/graph.ts
@@ -47,6 +47,73 @@ class Graph {
     return this;
   }
 
+  dfsRecursive(firstVertex: string | number) {
+    let visited: { [key: string | number]: boolean; } = {};
+    let result: (string | number)[] = [];
+    let adjacentList = this.adjacentList;
+
+    function helper(vertex: string | number | undefined) {
+      if(!vertex) return;
+
+      visited[vertex] = true;
+      result.push(vertex);
+
+      for(let v of adjacentList[vertex]) {
+        if(!visited[v]) {
+          helper(v);
+        }
+      }
+    }
+
+    helper(firstVertex);
+
+    return result;
+  }
+
+  dfsIterative(vertex: string | number) {
+    let stack: (string | number)[] = [vertex];
+    let result: (string | number)[] = [];
+    let visited: { [key: string | number]: boolean; } = {};
+
+    visited[vertex] = true;
+
+    while(stack.length > 0) {
+      const v = stack.pop()!;
+      result.push(v);
+
+      this.adjacentList[v].forEach(e => {
+        if(!visited[e]) {
+          visited[e] = true;
+          stack.push(e);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  bfs(vertex: string | number) {
+    let queue = [vertex];
+    let visited: { [key: string | number]: boolean; } = {};
+    let result: (string | number)[] = [];
+
+    visited[vertex] = true;
+
+    while(queue.length) {
+      const v = queue.shift()!;
+      result.push(v);
+
+      this.adjacentList[v].forEach(e => {
+        if(!visited[e]) {
+          visited[e] = true;
+          queue.push(e);
+        }
+      });
+    }
+
+    return result;
+  }
+
   print() {
     for(let key in this.adjacentList) {
       if(this.adjacentList[key].length === 0) {
@@ -59,5 +126,8 @@ class Graph {
   }
 }
 
-let graph = new Graph().addVertex("Tokyo").addVertex("Osaka").addEdge("Tokyo", "Osaka").removeVertex("Tokyo");
+let graph = new Graph().addVertex("A").addVertex("B").addVertex("C").addVertex("D").addVertex("E").addVertex("F").addEdge("A", "B").addEdge("B", "D").addEdge("D", "F").addEdge("D", "E").addEdge("F", "E").addEdge("E", "C").addEdge("C", "A");
 graph.print();
+console.log(graph.dfsRecursive("A"));
+console.log(graph.dfsIterative("A"));
+console.log(graph.bfs("A"));
